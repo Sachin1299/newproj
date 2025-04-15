0bfdee82fd838987d94e8f8a099d5300ce8405cb
Sachin1299 <chourasiyasachin12@gmail.com>
2025-04-15 23:43:33 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavScroll from './Components/NavScroll';
+
+function App() {
+  return (
+    <>
+    <NavScroll/>
+    </>
+  );
+}
+
+export default App;
